14cd76c8a8fc1c47ef57c281548028c72806f598
Jake Bolton <ninmonkeys@gmail.com>
2013-10-04 04:20:10 -0500

    initial styles, and includes set up

---
+++ arrays.php
@@ -0,0 +1,8 @@
+<?php
+$page_title = "arrays";
+include_once('./includes/base.php');
+
+echo "</p>hi</p>";
+
+include_once('./includes/footer.php');
+?>
---
+++ includes/base.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo "$page_title"; ?></title>
+		<link rel='stylesheet' id='main-css'  href='css/main.css' type='text/css' media='screen, print' />
+	</head>
+<body>
+
+<h1>$<?php echo "$page_title"; ?></h1>
